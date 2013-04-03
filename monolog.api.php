2e3dfc7bbf2e8ff53b1dbab0da0ed3eefb202991
Chris Pliakas <cpliakas@266779.no-reply.drupal.org>
2013-04-03 11:09:30 -0400

    Issue #1959986 by cpliakas: Provided defaults for the "settings callback" and "loader callback" options.

--- monolog.api.php
+++ monolog.api.php
@@ -46,40 +46,26 @@ function hook_monolog_handler_path() {
 }
 
 /**
- * Contains default profile configurations.
- *
- * A profile is a collection of handlers that process the record.
- */
-function hook_default_monolog_profiles() {
-  $profiles = array();
-
-  $profile = new stdClass();
-  $profile->disabled = FALSE;
-  $profile->api_version = 1;
-  $profile->name = 'syslog';
-  $profile->options = array(
-    'label' => 'Syslog',
-    'handlers' => array(
-      'syslog' => array(
-        'handler' => 'syslog',
-        'label' => 'Syslog',
-        'ident' => 'drupal',
-        'level' => 200,
-        'bubble' => 1,
-        'weight' => -50,
-      ),
-    ),
-  );
-  $profiles[$profile->name] = $profile;
-
-  return $profiles;
-}
-
-/**
  * Defines monolog handlers.
  *
  * Handlers process the records and write them to various sources such as files
  * remote servers, sockets, etc.
+ *
+ * @return array
+ *   An associative array keyed by unique name of the handler. Each handler is
+ *   an associative array containing:
+ *   - label: The human readable name of the handler displayed in admin pages.
+ *   - description: The description of the handler displayed in admin pages.
+ *   - default settings: An associative array of defaults for the settings
+ *     defined in the "settings callback".
+ *   - loader callback: The name of the function that loads the handler class.
+ *     Defaults to `monolog_HANDLER_NAME_handler_loader`.
+ *   - settings callback: The name of the function that adds additional settings
+ *     to the handler's configuration form. Pass FALSE if no additional settings
+ *     are required. Defaults to `monolog_HANDLER_NAME_handler_settings`.
+ *   - handler file: The path to the include file containing the loader and
+ *     settings callbacks. If the module implements hook_monolog_handler_path(),
+ *     this option defaults to `MODULE_PATH/HANDLER_PATH/HANDLER_NAME.inc`.
  *
  * @see https://github.com/Seldaek/monolog#handlers
  */
@@ -89,11 +75,16 @@ function hook_monolog_handler_info() {
   $handlers['stream'] = array(
     'label' => t('Stream Handler'),
     'description' => t('Logs records into any PHP stream, use this for log files.'),
-    'loader callback' => 'mymodule_stream_handler_loader',
-    'settings callback' => 'mymodule_stream_handler_settings',
     'default settings' => array(
       'filepath' => 'public://monolog/drupal.log',
     ),
+
+    // "loader callback" defaults to `monolog_stream_handler_loader`.
+
+    // "settings callback" defaults to `monolog_stream_handler_settings`.
+
+    // "handler file" defaults to `MODULE_PATH/handlers/stream.inc` since the
+    // hook_monolog_handler_path() returns "handlers".
   );
 
   return $handlers;
@@ -130,6 +121,36 @@ function monolog_stream_handler_settings(&$form, &$form_state, array $handler) {
     '#default_value' => $handler['filepath'],
     '#description' => t('The path or URI that the log file will be written to.'),
   );
+}
+
+/**
+ * Contains default profile configurations.
+ *
+ * A profile is a collection of handlers that process the record.
+ */
+function hook_default_monolog_profiles() {
+  $profiles = array();
+
+  $profile = new stdClass();
+  $profile->disabled = FALSE;
+  $profile->api_version = 1;
+  $profile->name = 'syslog';
+  $profile->options = array(
+    'label' => 'Syslog',
+    'handlers' => array(
+      'syslog' => array(
+        'handler' => 'syslog',
+        'label' => 'Syslog',
+        'ident' => 'drupal',
+        'level' => 200,
+        'bubble' => 1,
+        'weight' => -50,
+      ),
+    ),
+  );
+  $profiles[$profile->name] = $profile;
+
+  return $profiles;
 }
 
 /**
